746d78cc476f87ca5149aae90d65004ee441fca3
marysalvatore <104985627+marysalvatore@users.noreply.github.com>
2024-10-05 11:07:16 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 import Head from 'next/head';
 
 export const metadata = {
-  title: "Swift Main Solver",
-  description: "The Swift Main Solver for Everyone!",
+  title: "Swift Dapp Solver",
+  description: "The Swift Dapp Solver for Everyone!",
 };
 
 export default function RootLayout({ children }) {
